3355eedbbbe96777dea9e8c14bfea50ecb4060d7
Joe Guzzardo <Joe@Guzzardo.com>
2024-11-13 20:16:18 -0600

    sixth commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ dependencies {
 
     implementation(libs.material3)
     //implementation(libs.material3)
-    implementation("com.google.android.material:material:1.12.0")
-    //implementation("com.google.android.material:material:1.3.0")
+    //implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.3.0")
 
     //implementation("androidx.compose.material3:material3-window-size-class:1.3.1")
     //implementation(libs.androidx.material3.window.size.class)
