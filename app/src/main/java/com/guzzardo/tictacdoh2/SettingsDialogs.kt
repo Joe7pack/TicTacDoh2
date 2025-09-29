b6114849af11c6f3f998a6eb0510aed6d9a03560
Joe Guzzardo <Joe@Guzzardo.com>
2025-09-28 21:37:38 -0500

    celebrate autumnal equinox 2025

--- app/src/main/java/com/guzzardo/tictacdoh2/SettingsDialogs.kt
+++ app/src/main/java/com/guzzardo/tictacdoh2/SettingsDialogs.kt
@@ -416,11 +416,11 @@ class SettingsDialogs : Activity(), ToastMessage {
 
     companion object {
         private lateinit var resources: Resources
-        private var mMoveModeTouch = false //false = drag move mode; true = touch move mode
-        private var mMoveModeChecked = 0 // 0 = drag move mode; 1 = touch move mode
+        private var mMoveModeTouch = true // false = drag move mode; true = touch move mode
+        private var mMoveModeChecked = 1 // 0 = drag move mode; 1 = touch move mode
         private var mSoundModeChecked = 0 // 0 = sound on; 1 = sound off
+        private var mSoundMode = true // false = no sound; true = sound
         private var mDistanceMeasureChecked = 0 // 0 = miles; 1 = kilometers
-        private var mSoundMode = true //false = no sound; true = sound
         private var mDistanceUnitOfMeasure = "M"
     }
 }
